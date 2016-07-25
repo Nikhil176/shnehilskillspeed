7b834a8cc08dcea0f8237b27e130d79d008cb2c2
shnehil1 <shnehilshandilya@gmail.com>
2016-07-25 16:15:38 +0530

    tested

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,9 +42,9 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
- config.action_mailer.default_url_options = { :host => 'localhost:3001'}
- config.action_mailer.delivery_method = :smtp
- config.action_mailer.perform_deliveries = true
+ #config.action_mailer.default_url_options = { :host => 'localhost:3001'}
+ #config.action_mailer.delivery_method = :smtp
+ #config.action_mailer.perform_deliveries = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
